1b2b8928f28bb790451616c00bc94289cfacf5a3
groovestation31785 <angroover@gmail.com>
2017-01-22 14:52:13 -0800

    Add files via upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyBlog::Application.config.secret_key_base = 'aa0d194ec93d69bf3c9710667f59852730371812eb09756a9e28412d3bd0dac46244c4384655762830953100515c0b69a8a65e1879d2d0e71c882ce0f668a54c'
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+MyBlog::Application.routes.draw do
+  resources :posts
+
+  # get '/posts' => 'posts#index'
+  # get '/posts/new' => 'posts#new', as: 'new_post'
+  # post '/posts' => 'posts#create'
+  # get '/posts/:id' => 'posts#show', as: 'post'
+  # get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  # patch '/posts/:id' => 'posts#update'
+  # delete '/posts/:id' => 'posts#destroy'
+end
