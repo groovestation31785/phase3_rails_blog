4f45b9c86d3a51e717bf479a1aaab8c28870fea0
Allyse Groover <angroover@gmail.com>
2017-01-22 18:01:36 -0800

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 MyBlog::Application.routes.draw do
-  resources :posts
+  # resources :posts
 
-  # get '/posts' => 'posts#index'
-  # get '/posts/new' => 'posts#new', as: 'new_post'
-  # post '/posts' => 'posts#create'
-  # get '/posts/:id' => 'posts#show', as: 'post'
-  # get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
-  # patch '/posts/:id' => 'posts#update'
-  # delete '/posts/:id' => 'posts#destroy'
+  root 'posts#index'
+  get '/posts' => 'posts#index'
+  get '/posts/new' => 'posts#new', as: 'new_post'
+  post '/posts' => 'posts#create'
+  get '/posts/:id' => 'posts#show', as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch '/posts/:id' => 'posts#update'
+  delete '/posts/:id' => 'posts#destroy'
 end
